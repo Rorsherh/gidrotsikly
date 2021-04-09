7c8ccea5082f50cdb6b2b3909c5b818e6270df57
Alex <aekuz89@gmail.com>
2021-04-10 01:23:15 +0700

    сверстаны первые для блока формы

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-    
+
+        $('.search-form__item-suptitle').on('click',function(){
+        $(this).toggleClass('search-form__item-suptitle--active');
+        $(this).next().slideToggle();
+    })
 });
